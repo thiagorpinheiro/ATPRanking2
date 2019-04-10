cfcc760b87922f141c66955c9e6a561148485d53
THIAGO ROLIM PINHEIRO <thiago.rpinheiro1@senacsp.edu.br>
2019-04-10 12:19:08 -0300

    Initial commit

---
+++ app/src/main/java/sp/senac/br/atpranking/Pagina2Activity.java
@@ -0,0 +1,13 @@
+package sp.senac.br.atpranking;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Pagina2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pagina2_activity);
+    }
+}
---
+++ app/src/main/java/sp/senac/br/atpranking/borda_Activity.java
@@ -0,0 +1,13 @@
+package sp.senac.br.atpranking;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class borda_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.borda_layout);
+    }
+}
